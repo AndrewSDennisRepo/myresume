e2bda207233582fc11753f62b249cf5bc2ea93e1
AndrewsDennis55 <37302077+syphercrypt@users.noreply.github.com>
2018-06-16 17:12:54 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+
+function changecolorname(id){
+
+    document.getElementById(id).style.color= "white";
+
+}
+
+function changebackgroundcolor(id){
+
+    document.getElementById(id).style.backround = "Blue";
+
+}
